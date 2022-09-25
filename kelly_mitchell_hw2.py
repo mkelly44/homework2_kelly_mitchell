24a123bd648d81a5791fd27a37a027cdb4f670a1
Mitchell Kelly <mitchellkelly@c0711a-s30.ufhpc>
2022-09-25 17:37:42 -0400

    adds altitude for 24hr, 90min, and 45 min

--- kelly_mitchell_hw2.py
+++ kelly_mitchell_hw2.py
@@ -8,4 +8,12 @@ M = 5.97*10**24
 h = ((G*M*(T**2)/(4*pi**2))**(1/3)) - R
 #altitude formula from part a
 
+h1440 = ((G*M*(86400**2)/(4*pi**2))**(1/3)) - R
+h90 = ((G*M*(5400**2)/(4*pi**2))**(1/3)) - R
+h45 = ((G*M*(3600**2)/(4*pi**2))**(1/3)) - R
+
 print("The altitude of the satellite is",h,"meters")
+
+print("The altitude of a satellite that orbits in a day is",h1440,"meters")
+print("The altitude of a satellite that orbits in 90 minutes is",h90,"meters")
+print("The altitude of a satellite that orbits in 60 minutes is",abs(h45),"meters")
