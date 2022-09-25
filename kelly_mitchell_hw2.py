b11f0921b728b30f17d35f2260b2bcce42547741
Mitchell Kelly <mitchellkelly@c0711a-s30.ufhpc>
2022-09-25 17:03:29 -0400

    corrects time to time of orbit and explains where the altitude formula came from

--- kelly_mitchell_hw2.py
+++ kelly_mitchell_hw2.py
@@ -1,9 +1,11 @@
 from math import pi
 
-T = float(input("Enter time (s): "))
+T = float(input("Enter time of orbit (s): "))
 R = 6371*10**3   #km to m conversion
 G = 6.67*10**-11
 M = 5.97*10**24
 
 h = ((G*M*(T**2)/(4*pi**2))**(1/3)) - R
+#altitude formula from part a
+
 print("The altitude of the satellite is",h,"meters")
