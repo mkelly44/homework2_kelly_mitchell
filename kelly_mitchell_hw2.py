4b44486bc2b4a01a90c862ab3650f2cd7728138c
Mitchell Kelly <mitchellkelly@c0713a-s30.ufhpc>
2022-09-26 15:00:27 -0400

    adds text about altitudes and finalizes code with polishing

--- kelly_mitchell_hw2.py
+++ kelly_mitchell_hw2.py
@@ -5,15 +5,24 @@ R = 6371*10**3   #km to m conversion
 G = 6.67*10**-11
 M = 5.97*10**24
 
-h = ((G*M*(T**2)/(4*pi**2))**(1/3)) - R
-#altitude formula from part a
+h = ((G*M*(T**2)/(4*pi**2))**(1/3)) - R      #altitude formula from part a
 
-h1440 = ((G*M*(86400**2)/(4*pi**2))**(1/3)) - R
-h90 = ((G*M*(5400**2)/(4*pi**2))**(1/3)) - R
-h45 = ((G*M*(3600**2)/(4*pi**2))**(1/3)) - R
+h1440 = ((G*M*(86400**2)/(4*pi**2))**(1/3)) - R    #T = 1 Day
+h90 = ((G*M*(5400**2)/(4*pi**2))**(1/3)) - R       #T = 90 Minutes
+h45 = ((G*M*(3600**2)/(4*pi**2))**(1/3)) - R       #T = 45 Minutes
 
-print("The altitude of the satellite is",h,"meters")
+# Orbital Periods less than 5063 seconds produce an unphysical altitude which would be inside the radius of the Earth
 
-print("The altitude of a satellite that orbits in a day is",h1440,"meters")
-print("The altitude of a satellite that orbits in 90 minutes is",h90,"meters")
-print("The altitude of a satellite that orbits in 60 minutes is",abs(h45),"meters")
+if T<=5063.38783:  
+   print("This orbital period is too small")
+
+else:
+    print("The altitude of the satellite is",h,"meters")
+    print("The altitude of a satellite that orbits in a day is",h1440,"meters")
+    print("The altitude of a satellite that orbits in 90 minutes is",h90,"meters")
+    print("The altitude of a satellite that orbits in 45 minutes is",h45,"meters")
+
+
+# We can conclude that satellites cannot have an orbital period of 45 minutes, and orbital periods of around 90 minutes are typical of low earth orbiting satelites 
+
+
